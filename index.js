356f1b021ff6ceb5d569bb83a4a24446bd40e624
Alex Day <princeofpersiaa3.ag@gmail.com>
2018-01-28 17:49:52 +0530

    Update index.js
    Code fixes

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 var person = "<Enter Full Or Part of UserName or Number displayed in chat boxes>"
-var observer;
 
-(function(person, observer) {
+var observer = (function(person, observer) {
+    function makeVanish() {
+        let elements = document.querySelectorAll(".emoji-text-clickable")
+        if (elements && elements.length > 0) {
+            Array.from(elements)
+                    .filter(e => e.innerText.match( new RegExp(person.split(" ").join("|")) ))
+                    .forEach(e => e.parentElement.parentElement.remove())
+        }
+    }
+    
     const chatbox =  document.querySelector(".pane-chat-msgs pane-chat-body lastTabIndex".split(" ").join("."));
     const config = { childList: true, subtree: true }
 
-
+    if(!chatbox) return;
     const callback = (mutationList) => {
         for(mut of mutationList) {
             if(mutation.type === 'subtree') {
-                Array.from(document.querySelectorAll(".emoji-text-clickable"))
-                    .filter(e => e.innerText.match( new RegExp(person.split(" ").join("|")) ))
-                    .each(e => e.parentElement.parentElement.remove())
+                makeVanish()
             }
         }
     }
 
+    makeVanish();
     observer = new MutationObserver(callback);
     observer.observe(chatbox, config);
-})(person, observer)
+    return observer;
+})(person)
 
 // un comment this code to stop
 // if(observer && observer.disconnect) { observer.disconnect(); }
